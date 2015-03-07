1b9049ac0342df9585090aabf531ffca84b01098
Kiran Kumar Veeravelly <dev_supplylines@gmail.com>
2015-03-07 06:04:10 -0500

    updates

--- src/main/java/edu/earch/services/odata/GenericOdataServices.java
+++ src/main/java/edu/earch/services/odata/GenericOdataServices.java
@@ -16,6 +16,8 @@ import main.java.edu.earch.models.SimpleRequest;
 
 import org.core4j.Enumerable;
 import org.odata4j.consumer.ODataConsumer;
+import org.odata4j.consumer.behaviors.OClientBehavior;
+import org.odata4j.consumer.behaviors.OClientBehaviors;
 import org.odata4j.core.EntitySetInfo;
 import org.odata4j.core.OEntity;
 import org.odata4j.core.OProperty;
@@ -58,10 +60,11 @@ public class GenericOdataServices {
 		String endpoint = request.getEndPoint();
 		System.out.println(request.getEndPoint() + "---->"
 				+ request.getCategory());
-		ODataConsumer c = ODataJerseyConsumer.newBuilder(endpoint).build()
+		OClientBehavior behavior = OClientBehaviors.rateLimit(100);
+		ODataConsumer c = ODataJerseyConsumer.newBuilder(endpoint).setClientBehaviors(behavior).build()
 				.create(endpoint);
-		Enumerable<OEntity> entities = c.getEntities(request.getCategory())
-				.execute();
+		Enumerable<OEntity> entities = c.getEntities(request.getCategory()).top(100).
+				execute();
 		List<List<SimpleObject>> lstToReturn = new ArrayList<List<SimpleObject>>();
 		for (OEntity entity : entities) {
 			List<OProperty<?>> set = entity.getProperties();
@@ -69,8 +72,13 @@ public class GenericOdataServices {
 			for (OProperty<?> p : entity.getProperties()) {
 				Object v = p.getValue();
 				if (p.getType().equals(EdmSimpleType.BINARY) && v != null) {
-					v = org.odata4j.repack.org.apache.commons.codec.binary.Base64
-							.encodeBase64String((byte[]) v).trim();
+//					byte[] out = org.odata4j.repack.org.apache.commons.codec.binary.Base64
+//							.encodeBase64((byte[]) v);
+					byte[] out = (byte[]) v;
+					byte[] data = new byte[out.length - 78];
+					System.arraycopy(out, 78, data, 0, out.length - 78); 
+					v = new String(org.odata4j.repack.org.apache.commons.codec.binary.Base64
+							.encodeBase64(data));
 				}
 				SimpleObject objAdd = null;
 				if(v!=null){
@@ -97,10 +105,10 @@ public class GenericOdataServices {
 	public Response getCatalog() {
 		List<SimpleElement> lstToReturn = new ArrayList<SimpleElement>();
 		lstToReturn.add(new SimpleElement("North Wind","http://services.odata.org/Northwind/Northwind.svc/"));
-		lstToReturn.add(new SimpleElement("OData Test Service","http://services.odata.org/OData/OData.svc/"));
 		lstToReturn.add(new SimpleElement("Nuget","http://packages.nuget.org/v1/FeedService.svc/"));
 		lstToReturn.add(new SimpleElement("Ineta","http://live.ineta.org/InetaLiveService.svc/"));
 		lstToReturn.add(new SimpleElement("Nerd Dinner","http://www.nerddinner.com/Services/OData.svc/"));
+		lstToReturn.add(new SimpleElement("OData Test Service","http://services.odata.org/OData/OData.svc/"));
 		lstToReturn.add(new SimpleElement("Cambridge weather","http://odata.pyslet.org/weather/"));
 		lstToReturn.add(new SimpleElement("Socttsdale Arizona", "http://data.scottsdaleaz.gov/Finance/BusinessLicenses.svc/"));
 		
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,4 +1,13 @@
-var odataControllers = angular.module('odataControllers', ['ngAnimate']);
+var odataControllers = angular.module('odataControllers', ['ngAnimate','ngProgress']);
+
+odataControllers.run(function($rootScope, ngProgress) {
+  $rootScope.$on('$routeChangeStart', function(ev,data) {
+    ngProgress.start();
+  });
+  $rootScope.$on('$routeChangeSuccess', function(ev,data) {
+    //ngProgress.complete();
+  });
+});
 
 odataControllers.config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
@@ -10,34 +19,43 @@ odataControllers.factory('Data', function () {
     return { url: 'http://services.odata.org/Northwind/Northwind.svc/',category:'' };
 });
 
-odataControllers.controller('LevelOneController', ['$scope', '$http','$location','Data',function($scope, $http,$location,Data) {
+odataControllers.controller('LevelOneController', ['$scope', '$http','$location','Data','ngProgress',function($scope, $http,$location,Data,ngProgress) {
   $scope.Data=Data;
+  ngProgress.start();
   $http.post('/app/odata/getFirstLevelEntities',Data.url).success(function(data) {
     $scope.levelOneEntities = data;
-  });
+  }).error(function(data, status, headers, config) {
+			$scope.errorobject=data;
+		  });
   $scope.openLevelTwo=function(category){
   	$scope.Data.category=category;
+  	ngProgress.complete();
   	$location.path('/odata/leveltwo');
   }
 }]);
 
-odataControllers.controller('LevelTwoController', ['$scope', '$http','$routeParams','Data', function($scope, $http, $routeParams,Data) {
+odataControllers.controller('LevelTwoController', ['$scope', '$http','$routeParams','Data','ngProgress', function($scope, $http, $routeParams,Data,ngProgress) {
   $scope.Data = Data;
   $scope.objToPost = {};
   $scope.objToPost.endPoint = Data.url;
   $scope.objToPost.category = Data.category;
-  
+  ngProgress.start();
   $http.post('/app/odata/getSecondLevelEntities', $scope.objToPost).success(function(data) {
    		$scope.levelTwoEntities = data;
-  });
+   		ngProgress.complete();
+  }).error(function(data, status, headers, config) {
+			$scope.errorobject=data;
+		  });
 }]);
 
-odataControllers.controller('HomeController',['$scope','$http','$location','Data',function($scope, $http,$location,Data) {
+odataControllers.controller('HomeController',['$scope','$http','$location','Data','ngProgress',function($scope, $http,$location,Data,ngProgress) {
 	$scope.Data=Data;
 	$scope.label='URL of the oData Service';
 	$scope.catalog={};
+	ngProgress.start();
 	$http.get('/app/odata/catalog').success(function(data) {
 		$scope.catalog = data;
+		ngProgress.complete();
 	});
     $scope.openService=function(input){
       $scope.Data.url = input;
